32ed3e6b0eee4dee145c4d1f7541b7e9d2bb4ae3
Anastasia <kulevich05@gmail.com>
2022-12-09 13:49:20 +0300

    Use exp() instead of pow().

---
+++ model.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <math.h>
+#include "radioactivity.h"
+
+double
+model(double *radioactivity, double *time, int N, double point)
+{
+        // Value of the equation in given point.
+        double ret = 0;
+
+        for (int i = 0; i < N; i++) {
+                ret += time[i] * exp(-time[i]/point) * 
+                       (radioactivity[i] - exp(-time[i]/point));
+        }
+
+        return ret;
+}
+
+double
+nonlinear_equation(double *radioactivity, double *time, 
+                   int N, double precision)
+{
+        // Variable to find.
+        double decay_time = 0;
+
+        // Get interval.
+        double t1 = 0;
+        double t2 = 0;
+
+        printf("Enter the interval:\n");
+        printf(" $ ");
+        scanf("%lf", &t1);
+        printf(" $ ");
+        scanf("%lf", &t2);
+
+        while (fabs(t1 - t2) > precision) {
+                decay_time = (t1 + t2) / 2;
+                if (model(radioactivity, time, N, t1) * model(radioactivity, time, N, decay_time) < 0)
+                        t2 = decay_time;
+                else 
+                        t1 = decay_time;
+        }
+
+        return decay_time;
+}
+
+double 
+linear_equation (double *radioactivity, double *time, int N)
+{
+        // Return value.
+        double decay_rate = 0;
+
+        // Numerator.
+        double numer = 0;
+        // Denominator.
+        double denom = 0;
+
+        for (int i = 0; i < N; i++) {
+                numer += time[i];
+                denom += time[i] * (1 - radioactivity[i]);
+        }
+
+        decay_rate = numer / denom;
+
+        return decay_rate;
+}
+
