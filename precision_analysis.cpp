a1c2079a0b24352470558bdde5a0a1f3767d8c4e
IShatrov <111282652+IShatrov@users.noreply.github.com>
2022-12-09 14:27:23 +0300

    Add files via upload

--- precision_analysis.cpp
+++ precision_analysis.cpp
@@ -4,6 +4,7 @@
 
 const int INITIAL_SIZE = 10;
 const double precision = 1e-3;
+const double MAX_PRECISION_RATIO = 2;
 
 double precision_analysis(double *radioactivity, double *time, int N)
 {
@@ -12,22 +13,19 @@ double precision_analysis(double *radioactivity, double *time, int N)
 
     int current_N = INITIAL_SIZE;
 
-    double decay_time = NAN;
-    double decay_rate = NAN;
+    double decay_time = nonlinear_equation(radioactivity, time, N, precision);
+    double decay_rate = linear_equation(radioactivity, time, N);
 
     double deviation_exp = NAN;
     double deviation_lin = NAN;
 
     do
     {
-        decay_time = nonlinear_equation(radioactivity, time, current_N, precision);
-        decay_rate = linear_equation(radioactivity, time, current_N);
-
         deviation_exp = dev_exp(radioactivity, time, current_N, decay_time);
         deviation_lin = dev_linear(radioactivity, time, current_N, decay_rate);
 
         current_N++;
-    } while(current_N <= N && deviation_exp / deviation_lin < 2);
+    } while(current_N <= N && deviation_exp / deviation_lin < MAX_PRECISION_RATIO);
 
     return time[current_N - 1];
 }
@@ -41,12 +39,12 @@ double dev_exp(double *radioactivity, double *time, int M, double decay_time)
 
     for(int i = 0; i < M; i++)
     {
-        double num = radioactivity[i] - exp(-time[i]/decay_time);
+        double num = radioactivity[i] - exp(-time[i] / decay_time);
 
-        sum += num*num;
+        sum += num * num;
     }
 
-    double ans = sqrt(sum)/M;
+    double ans = sqrt(sum) / M;
 
     return ans;
 }
@@ -60,12 +58,12 @@ double dev_linear(double *radioactivity, double *time, int M, double decay_rate)
 
     for(int i = 0; i < M; i++)
     {
-        double num = radioactivity[i] - (1 - time[i]/decay_rate);
+        double num = radioactivity[i] - (1 - time[i] / decay_rate);
 
-        sum += num*num;
+        sum += num * num;
     }
 
-    double ans = sqrt(sum)/M;
+    double ans = sqrt(sum) / M;
 
     return ans;
 }
